33348ddfc74f770eb5bd706bffdb5f163234b2e0
avahmetozdemir <avamertozdemir@gmail.com>
2022-10-15 16:26:12 +0300

    20- add add two numbers problems solution in java

---
+++ Java/medium/addTwoNumbers.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode solution =  new ListNode(0);
+        ListNode p1 = l1 , p2 = l2 , current = solution;
+        int carry = 0;
+
+        while(p1 != null || p2 != null) {
+            int x = p1 !=null ? p1.val : 0 ;
+            int y = p2 !=null ? p2.val : 0 ;
+
+            int sum = x+y+carry;
+
+            carry = sum/10;
+
+            current.next =  new ListNode(sum%10);
+            current = current.next;
+
+            if(p1 != null) p1 = p1.next;
+            if(p2 != null) p2 = p2.next;
+        }
+
+        if(carry > 0){
+            current.next =  new ListNode(carry);
+        }
+
+        return solution.next;
+    }
+}
--- README.md
+++ README.md
@@ -312,3 +312,6 @@ You are given two non-empty linked lists representing two non-negative integers.
 
 You may assume the two numbers do not contain any leading zero, except the number 0 itself.
 
+**Time Complexity :** O(max(m.n))
+**Space Complexity :** O(max(m,n))
+m and n are size of l1 and l2
