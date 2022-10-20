1d94c523a9da015ab9db3e542698d96191f224bb
avahmetozdemir <avamertozdemir@gmail.com>
2022-10-20 10:26:42 +0300

    23- add palindrome number problems solution in java

---
+++ Java/easy/palindromeNumber.java
@@ -0,0 +1,16 @@
+class Solution{
+    public boolean isPalindrome(int x){
+        if(x < 0 || (x % 10 == 0 && x != 0)){
+            return false;
+        }
+
+        int reversed = 0;
+
+        while (x > reversed){
+            r = r * 10 + x % 10;
+            x = x/10;
+        }
+
+        return r == x;
+    }
+}
